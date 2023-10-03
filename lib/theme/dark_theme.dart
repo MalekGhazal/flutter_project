d334e1bd06758d6819a476e7aed9660d6e69fb3a
Malek Kazal <ghazal.malek@gmail.com>
2023-10-03 14:41:30 -0400

    Light/Dark Theme

---
+++ lib/theme/dark_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    appBarTheme: const AppBarTheme(backgroundColor: Color(0xFF203D4E)),
+    colorScheme: const ColorScheme.dark(
+        background: Color(0xFF203D4E),
+        primary: Color(0xFFEBE8E2),
+        secondary: Color(0xFFBD5F5F)));
---
+++ lib/theme/light_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+    appBarTheme: const AppBarTheme(backgroundColor: Color(0xFFEBE8E2)),
+    colorScheme: const ColorScheme.light(
+        background: Color(0xFFEBE8E2),
+        primary: Color(0xFF203D4E),
+        secondary: Color(0xFFBD5F5F)));
